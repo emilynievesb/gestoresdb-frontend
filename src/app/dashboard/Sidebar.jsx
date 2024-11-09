a8148b9c31ce5ca54a10be1a06597db178c98d3e
Emily Nieves <nemily1710@gmail.com>
2024-11-09 16:40:39 -0500

    feat: :sparkles: Links de redireccion a rutas

--- src/app/dashboard/Sidebar.jsx
+++ src/app/dashboard/Sidebar.jsx
@@ -1,22 +1,36 @@
+'use client';
 import React from 'react';
-import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 import { Home, Search } from 'lucide-react';
 import UserInfo from './UserInfo';
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return (
         <aside className="w-64 bg-gray-800 text-gray-100 flex flex-col justify-between p-4">
             <div>
                 <h2 className="text-xl font-semibold mb-6">Mi Dashboard</h2>
                 <nav className="space-y-4">
-                    <Button variant="ghost" className="w-full flex items-center gap-2 text-gray-400 hover:text-gray-800 hover:bg-gray-100">
+                    <Link
+                        href="/dashboard/boards"
+                        className={`w-full flex items-center gap-2 px-4 py-2 rounded-lg 
+              ${pathname === '/dashboard/boards' ? 'bg-gray-700 text-white' : 'text-gray-400 hover:bg-gray-700 hover:text-white'}
+            `}
+                    >
                         <Home className="w-5 h-5" />
                         <span>Mis tableros</span>
-                    </Button>
-                    <Button variant="ghost" className="w-full flex items-center gap-2 text-gray-400 hover:text-gray-800 hover:bg-gray-100">
+                    </Link>
+
+                    <Link
+                        href="/dashboard/search"
+                        className={`w-full flex items-center gap-2 px-4 py-2 rounded-lg 
+              ${pathname === '/dashboard/search' ? 'bg-gray-700 text-white' : 'text-gray-400 hover:bg-gray-700 hover:text-white'}
+            `}
+                    >
                         <Search className="w-5 h-5" />
                         <span>Buscar tablero</span>
-                    </Button>
+                    </Link>
                 </nav>
             </div>
             <UserInfo />
