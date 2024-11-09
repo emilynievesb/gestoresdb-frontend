2caf3561c505c37e4f10f42f25c9cf8dc2ff5e6d
Emily Nieves <nemily1710@gmail.com>
2024-11-09 17:52:47 -0500

    feat: :sparkles: Vista de tablero en específico

---
+++ src/app/dashboard/boards/[id]/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React, { useState } from 'react';
+import BoardView from './BoardView';
+
+const BoardPage = () => {
+    return (
+        <div className="p-8">
+            <BoardView />
+        </div>
+    );
+};
+
+export default BoardPage;
