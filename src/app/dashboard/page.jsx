6cfe22ef30cf4409af1e5533d66a635f178fdd7d
Emily Nieves <nemily1710@gmail.com>
2024-11-09 16:11:15 -0500

    feat: :sparkles: Se realiza page inicial para la ruta /dashboard

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const DashboardPage = () => {
+    return (
+        <>
+            <Header />
+            <p className="text-gray-600">Aquí podrás gestionar tus tableros y acceder a todas las funcionalidades de la plataforma.</p>
+        </>
+    );
+};
+
+export default DashboardPage;
